733a4e8a7452fafe5799e44806bfc9b78d6cb771
Gareth Smith <garethdanielsmith@gmail.com>
2018-10-13 14:48:41 +0100

    Add a code example to the readme.

--- test/Test/ReadmeExample.purs
+++ test/Test/ReadmeExample.purs
@@ -1,14 +1,6 @@
-An unfinished experimental SQL query builder for Purescript, loosely based on Java's Querydsl (http://www.querydsl.com/).
+-- Changes to the README.md code example should be done in here, then copy-pasted into there.
+module Test.QueryDsl.ReadmeExample where
 
-The goal is to allow building SQL in a type-safe and composable way, ideally with comprehensible types signatures.
-
-# Features
- - Various combinators for creating type-checked predicates for selects/deletes.
- - Column names and types checked for insertions.
- - Generates reasonably readable SQL for monitoring and debugging.
-
-# Example
-```purescript
 import Prelude
 import Effect.Aff (Aff)
 import QueryDsl (Column, Table, makeTable, from, select, where_, orderBy, limit, asc)
@@ -31,4 +23,3 @@ getLastNames conn = do
       `where_` (c.firstName :== "Bob")
       `orderBy` [asc c.id]
       `limit` 10
-```
