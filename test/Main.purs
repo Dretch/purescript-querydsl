04311251e65b6aa853ab16f3e0055c5811c28de8
Gareth Daniel Smith <garethdanielsmith@gmail.com>
2018-12-29 18:47:32 +0000

    Update to the latest versions of dependencies.

--- test/Main.purs
+++ test/Main.purs
@@ -3,10 +3,11 @@ module Test.Main where
 import Prelude
 
 import Effect (Effect)
+import Effect.Aff (launchAff_)
 import Test.QueryDsl as QueryDsl
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (run)
 
 main :: Effect Unit
-main = run [consoleReporter] do
+main = launchAff_ $ run [consoleReporter] do
   QueryDsl.test
