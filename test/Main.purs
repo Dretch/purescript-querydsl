b51c4a8a4cf4cc852dccab4c681438ea609cef95
Gareth Daniel Smith <garethdanielsmith@gmail.com>
2019-05-11 11:42:56 +0100

    Update to the latest purescript-spec

--- test/Main.purs
+++ test/Main.purs
@@ -6,8 +6,8 @@ import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Test.QueryDsl as QueryDsl
 import Test.Spec.Reporter.Console (consoleReporter)
-import Test.Spec.Runner (run)
+import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
-main = launchAff_ $ run [consoleReporter] do
+main = launchAff_ $ runSpec [consoleReporter] do
   QueryDsl.test
